338c97da60967a62ff79933364096faa0ac7d8f0
Fedor Tretyakov <fedor@softgrad.com>
2022-07-05 18:37:12 +0300

    fix ex3, fix time measurement condition

--- sort/exercises/simple_sort/ex2.php
+++ sort/exercises/simple_sort/ex2.php
@@ -9,4 +9,4 @@ $arr = [3,1,99,6,34,22,23,12,3];
 selectionSort($arr);
 echo json_encode($arr); // prints [1,3,3,6,12,22,23,34,99]
 
-// Measure the time required to sort 10000 numbers.
+// Measure the time required to sort 100000 numbers.
--- sort/exercises/simple_sort/ex3.php
+++ sort/exercises/simple_sort/ex3.php
@@ -2,11 +2,11 @@
 
 // Create a insertSortRev() function which uses the insertion sort algorithm to sort an array of numbers in descending order.
 
-function selectionSort() {}
+function insertSortRev() {}
 
 // Usage example:
 $arr = [3,1,99,6,34,22,23,12,3];
-selectionSort($arr);
+insertSortRev($arr);
 echo json_encode($arr); // prints [99,34,23,22,12,6,3,3,1]
 
-// Measure the time required to sort 10000 numbers.
+// Measure the time required to sort 100000 numbers.
